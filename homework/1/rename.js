f3b1caa85e18401315e2501332762ea459ff2382
ThinkHalo <1750367868@qq.com>
2022-01-09 10:28:42 +0800

    merge(/homework/1/rename.js): 完成作业1并ignore yarn.lock

--- homework/1/rename.js
+++ homework/1/rename.js
@@ -5,9 +5,28 @@ const traverse = require('../../common/traverse');
 function transform(root, originName, targetName) {
   // 遍历所有节点
   return traverse((node, ctx, next) => {
-
+    console.log(node);
     // TODO: 作业代码写在这里
-    if (node.type === 'xxx') {
+    // 重命名函数名 和 局部变量名
+    if (node.type === "FunctionDeclaration" || node.type === "VariableDeclarator") {
+      if(node.id.name == originName) {
+        node.id.name = targetName
+      }
+    }
+    // 重命名表达式两个操作数
+    if(node.type === "BinaryExpression" && node.operator === "+") {
+      if(node.left.name == originName) {
+        node.left.name = targetName;
+      }
+      if(node.right.name == originName){
+        node.right.name = targetName;
+      }
+    }
+    // 重命名对象值
+    if(node.type === "MemberExpression" && node.computed) {
+      if(node.object.object.object.name == originName) {
+        node.object.object.object.name = targetName;
+      }
     }
 
     // 继续往下遍历
