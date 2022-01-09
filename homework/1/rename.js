ee7e5b7d4ae84014c079a4944c6958a17738ea24
yuki <554737661@qq.com>
2022-01-09 19:12:23 +0800

    简化代码

--- homework/1/rename.js
+++ homework/1/rename.js
@@ -1,50 +1,47 @@
-const acorn = require("acorn");
-const astring = require("astring");
-const traverse = require("../../common/traverse");
+const acorn = require('acorn');
+const astring = require('astring');
+const traverse = require('../../common/traverse');
+const isNode = target =>
+  target && typeof target.type === 'string';
+const nodeMap= new Map([
+  //函数声明
+['FunctionDeclaration',['id']],
+//变量声明
+['VariableDeclarator',['id']],
+//二元运算表达式节点
+['BinaryExpression',['left','right']],
+//赋值表达式节点
+['AssignmentExpression',['left','right']],
+//逻辑运算表达式节点
+['LogicalExpression',['left','right']],
+//成员表达式节点
+['MemberExpression',['object']],
+//对象表达式中的属性节点
+['Property',['value']],
+//一元运算表达式节点
+['UnaryExpression',['argument']],
+//update 运算表达式节点(++,--)
+['UpdateExpression',['argument']],
+//条件表达式，三元运算表达式
+['ConditionalExpression',['alternate','consequent']],
+//函数调用表达式
+['CallExpression',['callee','arguments']],
+//逗号运算符构建的表达式
+['SequenceExpression',['expressions']],
+])
+
 
 function transform(root, originName, targetName) {
   // 遍历所有节点
   return traverse((node, ctx, next) => {
-    // TODO: 作业代码写在这里
-    switch (node.type) {
-      // 修改方法名
-      case "FunctionDeclaration":
-        if (node.id.name === originName) {
-          node.id.name = targetName;
+    if(nodeMap.has(node.type)){
+      let list=nodeMap.get(node.type);
+      for(const index in list){
+        let child=list[index];
+        if(node[child].type==='Identifier' && node[child].name===originName){
+          node[child].name=targetName;
         }
-        break;
-      //修改变量名
-      case "VariableDeclarator":
-        if (node.id.name === originName) {
-          node.id.name = targetName;
-        }
-        break;
-      //修改运算表达式时的变量
-      case "BinaryExpression":
-        if (node.left.type === "Identifier" && node.left.name === originName) {
-          node.left.name = targetName;
-        }
-        if (node.right.type === "Identifier" && node.right.name === originName) {
-          node.right.name = targetName;
-        }
-        break;
-      //表达式中需要的修改
-      case "MemberExpression":
-        if (!node.object.object && node.object.name === originName) {
-          node.object.name = targetName;
-        }
-        break;
-      //其他可能情况，接着case下去
-      //如修改赋值语句右侧可能的变量。
-      case "AssignmentExpression":
-        if (node.left.type === "Identifier" && node.left.name === originName) {
-          node.left.name = targetName;
-        }
-        if (node.right.type === "Identifier" && node.right.name === originName) {
-          node.right.name = targetName;
-        }
-        break;
-      default:
+      }
     }
 
     // 继续往下遍历
@@ -56,6 +53,7 @@ function rename(code, originName, targetName) {
   const ast = acorn.parse(code, {
     ecmaVersion: 5,
   });
+
   return astring.generate(transform(ast, originName, targetName));
 }
 
