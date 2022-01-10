93ae22f6a4cca8aba3e844b0653649ddd1a7faf9
Karshilov <karshilov@foxmail.com>
2022-01-10 15:22:24 +0800

    feat: finish hw1

--- homework/1/rename.js
+++ homework/1/rename.js
@@ -1,25 +1,33 @@
-const acorn = require('acorn');
-const astring = require('astring');
-const traverse = require('../../common/traverse');
+//@ts-check
+const acorn = require("acorn");
+const astring = require("astring");
+const traverse = require("../../common/traverse");
 
 function transform(root, originName, targetName) {
   // 遍历所有节点
   return traverse((node, ctx, next) => {
-
     // TODO: 作业代码写在这里
-    if (node.type === 'xxx') {
+    for (const k in node) {
+      if (
+        node[k] instanceof Object &&
+        !["label", "key", "property"].includes(k) &&
+        node[k].type === "Identifier" &&
+        node[k].name === originName
+      ) {
+        node[k].name = targetName;
+      }
     }
 
     // 继续往下遍历
-    return next(node, ctx)
+    return next(node, ctx);
   })(root);
 }
 
 function rename(code, originName, targetName) {
   const ast = acorn.parse(code, {
     ecmaVersion: 5,
-  })
-  return astring.generate(transform(ast, originName, targetName))
+  });
+  return astring.generate(transform(ast, originName, targetName));
 }
 
-module.exports = rename
+module.exports = rename;
