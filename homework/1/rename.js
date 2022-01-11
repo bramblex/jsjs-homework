de1ba913a56074e5546b917b0eb4c0c802a32b53
vecpeng <841298391@qq.com>
2022-01-11 22:50:27 +0800

    :sparkles:feat: rename variables in node AST tree

--- homework/1/rename.js
+++ homework/1/rename.js
@@ -2,14 +2,42 @@ const acorn = require('acorn');
 const astring = require('astring');
 const traverse = require('../../common/traverse');
 
+const FunctionDeclaration = "FunctionDeclaration";
+const VariableDeclarator = "VariableDeclarator";
+const MemberExpression = "MemberExpression";
+const Identifier = "Identifier";
+const BinaryExpression = "BinaryExpression";
+
 function transform(root, originName, targetName) {
   // 遍历所有节点
   return traverse((node, ctx, next) => {
 
-    // TODO: 作业代码写在这里
-    if (node.type === 'xxx') {
+    switch (node.type) {
+      // function bar()
+      case FunctionDeclaration:
+      // var bar
+      case VariableDeclarator:
+        console.log(node.type)
+        if (node.id.name === originName) {
+          node.id.name = targetName;
+        }
+        break;
+      // { foo: bar }
+      case MemberExpression:
+        if (node.object.type === Identifier && node.object.name === originName) {
+          node.object.name = targetName;
+        }
+        break;
+      // bar + bar
+      case BinaryExpression:
+        if (node.left.name === originName) {
+          node.left.name = targetName;
+        }
+        if (node.right.name === originName) {
+          node.right.name = targetName;
+        }
+        break;
     }
-
     // 继续往下遍历
     return next(node, ctx)
   })(root);
