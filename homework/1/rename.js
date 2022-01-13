a16044e44395ad4e6d4e2d6312b813330b8cf9d5
DawnMagnet <axccjqh@qq.com>
2022-01-13 18:04:53 +0800

    作业1与作业2

--- homework/1/rename.js
+++ homework/1/rename.js
@@ -1,25 +1,42 @@
-const acorn = require('acorn');
-const astring = require('astring');
-const traverse = require('../../common/traverse');
+const acorn = require("acorn");
+const astring = require("astring");
+const traverse = require("../../common/traverse");
 
 function transform(root, originName, targetName) {
   // 遍历所有节点
   return traverse((node, ctx, next) => {
-
     // TODO: 作业代码写在这里
-    if (node.type === 'xxx') {
+    if (node.type === "FunctionDeclaration") {
+      if (node.id.name === originName) {
+        node.id.name = targetName;
+      }
+    } else if (node.type === "VariableDeclaration") {
+      if (node.declarations[0].id.name === originName) {
+        node.declarations[0].id.name = targetName;
+      }
+    } else if (node.type === "MemberExpression" && !node.object.object) {
+      if (node.object.name === originName) {
+        node.object.name = targetName;
+      }
+    } else if (node.type === "BinaryExpression") {
+      if (node.left.name === originName) {
+        node.left.name = targetName;
+      }
+      if (node.right.name === originName) {
+        node.right.name = targetName;
+      }
     }
 
     // 继续往下遍历
-    return next(node, ctx)
+    return next(node, ctx);
   })(root);
 }
 
 function rename(code, originName, targetName) {
   const ast = acorn.parse(code, {
     ecmaVersion: 5,
-  })
-  return astring.generate(transform(ast, originName, targetName))
+  });
+  return astring.generate(transform(ast, originName, targetName));
 }
 
-module.exports = rename
+module.exports = rename;
--- homework/2/eval.js
+++ homework/2/eval.js
@@ -1,19 +1,65 @@
 const acorn = require('acorn');
 
 function evaluate(node, env) {
+  let res;
   switch (node.type) {
-    case 'Literal':
-    // TODO: 补全作业代码
+    case 'Literal': 
+      return node.value;
+    case 'LogicalExpression':
+      if (node.operator === '||') return evaluate(node.left, env) || evaluate(node.right, env)
+      if (node.operator === '&&') return evaluate(node.left, env) && evaluate(node.right, env)
+    case 'UnaryExpression':
+      return !evaluate(node.argument, env)
+    case 'BinaryExpression':
+      switch (node.operator) {
+        case "+": return evaluate(node.left, env) + evaluate(node.right, env);
+        case "-": return evaluate(node.left, env) - evaluate(node.right, env);
+        case "*": return evaluate(node.left, env) * evaluate(node.right, env);
+        case "/": return evaluate(node.left, env) / evaluate(node.right, env);
+        case "<=": return evaluate(node.left, env) <= evaluate(node.right, env);
+      }
+      throw new Error(`Unsupported BinaryExpression ${node.type} at Location ${node.start}:${node.end}`);
+    case 'ObjectExpression':
+      res = {}
+      for (const item of node.properties) {
+        res[item.key.name] = evaluate(item.value, env);
+      }
+      return res;
+    // case 'ObjectExpression':
+    //   return node.properties.reduce((acc, property) => ({ ...acc, [property.key.name]: evaluate(property.value, env) }), {});
+    case 'ConditionalExpression':
+      let condition = evaluate(node.test, env)
+      if (condition) return evaluate(node.consequent, env)
+      else return evaluate(node.alternate, env)
+    case 'ArrayExpression':
+      res = []
+      for (let item of node.elements) {
+        res.push(evaluate(item, env))
+      }
+      return res;
+    case 'CallExpression':
+      const callee = evaluate(node.callee, env);
+      const args = node.arguments.map(arg => evaluate(arg, env));
+      return callee(...args);
+    case 'ArrowFunctionExpression':
+      return (...args) => {
+        return evaluate(node.body, {
+          ...env,
+          ...node.params.reduce((acc, param, index) => ({ ...acc, [param.name]: args[index] }), {}),
+        });
+      };
+    case 'Identifier':
+      return env[node.name];
   }
-
+  return node.type
   throw new Error(`Unsupported Syntax ${node.type} at Location ${node.start}:${node.end}`);
 }
 
-function eval(code, env = {}) {
+function customerEval(code, env = {}) {
   const node = acorn.parseExpressionAt(code, 0, {
     ecmaVersion: 6
   })
   return evaluate(node, env)
 }
 
-module.exports = eval
+module.exports = customerEval
