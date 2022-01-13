c4f8dbccb8c3ef2d6931e6e23bc4761466e42e41
DawnMagnet <axccjqh@qq.com>
2022-01-13 18:06:03 +0800

    格式优化

--- homework/2/eval.js
+++ homework/2/eval.js
@@ -1,65 +1,79 @@
-const acorn = require('acorn');
+const acorn = require("acorn");
 
 function evaluate(node, env) {
   let res;
   switch (node.type) {
-    case 'Literal': 
+    case "Literal":
       return node.value;
-    case 'LogicalExpression':
-      if (node.operator === '||') return evaluate(node.left, env) || evaluate(node.right, env)
-      if (node.operator === '&&') return evaluate(node.left, env) && evaluate(node.right, env)
-    case 'UnaryExpression':
-      return !evaluate(node.argument, env)
-    case 'BinaryExpression':
+    case "LogicalExpression":
+      if (node.operator === "||")
+        return evaluate(node.left, env) || evaluate(node.right, env);
+      if (node.operator === "&&")
+        return evaluate(node.left, env) && evaluate(node.right, env);
+    case "UnaryExpression":
+      return !evaluate(node.argument, env);
+    case "BinaryExpression":
       switch (node.operator) {
-        case "+": return evaluate(node.left, env) + evaluate(node.right, env);
-        case "-": return evaluate(node.left, env) - evaluate(node.right, env);
-        case "*": return evaluate(node.left, env) * evaluate(node.right, env);
-        case "/": return evaluate(node.left, env) / evaluate(node.right, env);
-        case "<=": return evaluate(node.left, env) <= evaluate(node.right, env);
+        case "+":
+          return evaluate(node.left, env) + evaluate(node.right, env);
+        case "-":
+          return evaluate(node.left, env) - evaluate(node.right, env);
+        case "*":
+          return evaluate(node.left, env) * evaluate(node.right, env);
+        case "/":
+          return evaluate(node.left, env) / evaluate(node.right, env);
+        case "<=":
+          return evaluate(node.left, env) <= evaluate(node.right, env);
       }
-      throw new Error(`Unsupported BinaryExpression ${node.type} at Location ${node.start}:${node.end}`);
-    case 'ObjectExpression':
-      res = {}
+      throw new Error(
+        `Unsupported BinaryExpression ${node.type} at Location ${node.start}:${node.end}`
+      );
+    case "ObjectExpression":
+      res = {};
       for (const item of node.properties) {
         res[item.key.name] = evaluate(item.value, env);
       }
       return res;
     // case 'ObjectExpression':
     //   return node.properties.reduce((acc, property) => ({ ...acc, [property.key.name]: evaluate(property.value, env) }), {});
-    case 'ConditionalExpression':
-      let condition = evaluate(node.test, env)
-      if (condition) return evaluate(node.consequent, env)
-      else return evaluate(node.alternate, env)
-    case 'ArrayExpression':
-      res = []
+    case "ConditionalExpression":
+      let condition = evaluate(node.test, env);
+      if (condition) return evaluate(node.consequent, env);
+      else return evaluate(node.alternate, env);
+    case "ArrayExpression":
+      res = [];
       for (let item of node.elements) {
-        res.push(evaluate(item, env))
+        res.push(evaluate(item, env));
       }
       return res;
-    case 'CallExpression':
+    case "CallExpression":
       const callee = evaluate(node.callee, env);
-      const args = node.arguments.map(arg => evaluate(arg, env));
+      const args = node.arguments.map((arg) => evaluate(arg, env));
       return callee(...args);
-    case 'ArrowFunctionExpression':
+    case "ArrowFunctionExpression":
       return (...args) => {
         return evaluate(node.body, {
           ...env,
-          ...node.params.reduce((acc, param, index) => ({ ...acc, [param.name]: args[index] }), {}),
+          ...node.params.reduce(
+            (acc, param, index) => ({ ...acc, [param.name]: args[index] }),
+            {}
+          ),
         });
       };
-    case 'Identifier':
+    case "Identifier":
       return env[node.name];
   }
-  return node.type
-  throw new Error(`Unsupported Syntax ${node.type} at Location ${node.start}:${node.end}`);
+  return node.type;
+  throw new Error(
+    `Unsupported Syntax ${node.type} at Location ${node.start}:${node.end}`
+  );
 }
 
 function customerEval(code, env = {}) {
   const node = acorn.parseExpressionAt(code, 0, {
-    ecmaVersion: 6
-  })
-  return evaluate(node, env)
+    ecmaVersion: 6,
+  });
+  return evaluate(node, env);
 }
 
-module.exports = customerEval
+module.exports = customerEval;
--- homework/2/eval.test.js
+++ homework/2/eval.test.js
@@ -1,27 +1,28 @@
-const customerEval = require('./eval');
+const customerEval = require("./eval");
 
 function throwError() {
-  throw new Error('error')
+  throw new Error("error");
 }
 
-const baseEnv = { throwError }
+const baseEnv = { throwError };
 
-test('测试表达式 - 初级挑战', () => {
+test("测试表达式 - 初级挑战", () => {
   const sourceCodeList = [
-    '1 - 2 + 3 * 10 / 5',
-    'true && true || throwError()',
-    'false && throwError() || true',
-    'true ? 1 : throwError()',
-    'false ? throwError() : 2',
-    '({ a: 1 + 2 + 3, b: 4 + 5, c: [1, 2, 3] })',
-    '(f => f + 3)(3)'
+    "1 - 2 + 3 * 10 / 5",
+    "true && true || throwError()",
+    "false && throwError() || true",
+    "true ? 1 : throwError()",
+    "false ? throwError() : 2",
+    "({ a: 1 + 2 + 3, b: 4 + 5, c: [1, 2, 3] })",
+    "(f => f + 3)(3)",
   ];
   for (sourceCode of sourceCodeList) {
     expect(customerEval(sourceCode, baseEnv)).toStrictEqual(eval(sourceCode));
   }
-})
+});
 
-test('测试表达式 - 终极挑战', () => {
-  const sourceCode = '(f => (x => f (y => x (x) (y))) (x => f (y => x(x)(y))))(f => n => n <= 1 ? n : n * f(n - 1))(10)'
+test("测试表达式 - 终极挑战", () => {
+  const sourceCode =
+    "(f => (x => f (y => x (x) (y))) (x => f (y => x(x)(y))))(f => n => n <= 1 ? n : n * f(n - 1))(10)";
   expect(customerEval(sourceCode)).toStrictEqual(eval(sourceCode));
-})
+});
