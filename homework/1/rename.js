8fed55e436293402a457bc7cb011b6e6cfe050d1
wuuusicong <87551619@qq.com>
2022-01-14 23:19:54 +0800

    update

--- homework/1/rename.js
+++ homework/1/rename.js
@@ -6,9 +6,35 @@ function transform(root, originName, targetName) {
   // 遍历所有节点
   return traverse((node, ctx, next) => {
 
+
     // TODO: 作业代码写在这里
-    if (node.type === 'xxx') {
+
+    // if (node.type === 'FunctionDeclaration') {
+    //   if(node.id.name === originName){
+    //     node.id.name = targetName
+    //   }
+    // }
+    // if (node.type === 'LabeledStatement') {
+    //   if(node.label.name === originName){
+    //     node.label.name = targetName
+    //   }
+    // }
+    // if (node.type === 'VariableDeclaration') {
+    //   for(i of node.declarations){
+    //     i.id.name = targetName
+    //   }
+    // }
+    // if (node.type === 'BreakStatement') {
+    //   if(node.label.name === originName){
+    //     node.label.name = targetName
+    //   }
+    // }
+    if (node.type === 'Identifier') {
+      if(node.name === originName){
+        node.name = targetName
+      }
     }
+
 
     // 继续往下遍历
     return next(node, ctx)
