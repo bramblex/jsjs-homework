5b5c6c97fc2abf7b66b57e8b00580dab2ccce377
MrPluto0 <710852553@qq.com>
2022-01-14 15:17:52 +0800

    upload homework1&2

--- homework/1/rename.js
+++ homework/1/rename.js
@@ -1,25 +1,33 @@
-const acorn = require('acorn');
-const astring = require('astring');
-const traverse = require('../../common/traverse');
+const acorn = require("acorn");
+const astring = require("astring");
+const traverse = require("../../common/traverse");
 
 function transform(root, originName, targetName) {
   // 遍历所有节点
   return traverse((node, ctx, next) => {
+    // node can't belong to "label|key|property[computed=false]"
+    if (node.type === "Identifier") {
+      let label = ctx.label && ctx.label === node;
+      let key = ctx.key && ctx.key === node;
+      let property = ctx.property && !ctx.computed && ctx.property === node;
 
-    // TODO: 作业代码写在这里
-    if (node.type === 'xxx') {
+      if (label || key || property) {
+        return next(node, node);
+      } else if (node.name === originName) {
+        node.name = targetName;
+      }
     }
 
-    // 继续往下遍历
-    return next(node, ctx)
+    // 继续往下遍历, 传入ctx为当前node
+    return next(node, node);
   })(root);
 }
 
 function rename(code, originName, targetName) {
   const ast = acorn.parse(code, {
     ecmaVersion: 5,
-  })
-  return astring.generate(transform(ast, originName, targetName))
+  });
+  return astring.generate(transform(ast, originName, targetName));
 }
 
-module.exports = rename
+module.exports = rename;
--- homework/1/rename.test.js
+++ homework/1/rename.test.js
@@ -1,20 +1,26 @@
-const acorn = require('acorn');
-const traverse = require('../../common/traverse')
-const rename = require('./rename')
+/*
+ * @Descripttion:
+ * @Author: Gypsophlia
+ * @Date: 2022-01-09 23:52:04
+ * @LastEditTime: 2022-01-10 13:53:29
+ */
+const acorn = require("acorn");
+const traverse = require("../../common/traverse");
+const rename = require("./rename");
 
 function toStandard(code) {
-  const root = acorn.parse(code, { ecmaVersion: 5 })
+  const root = acorn.parse(code, { ecmaVersion: 5 });
 
   const target = traverse((node, ctx, next) => {
-    delete node.start
-    delete node.end
-    return next(node)
-  })(root)
+    delete node.start;
+    delete node.end;
+    return next(node);
+  })(root);
 
-  return target
+  return target;
 }
 
-test('测试重命名变量', () => {
+test("测试重命名变量", () => {
   const sourceCode = `
 function foo() {
 	foo: while(true) {
@@ -24,7 +30,7 @@ function foo() {
 		break foo;
 	}
 }
-`
+`;
 
   const targetCode = `
 function bar() {
@@ -37,7 +43,7 @@ function bar() {
 }
 `;
 
-  const result = rename(sourceCode, 'foo', 'bar');
-
+  const result = rename(sourceCode, "foo", "bar");
+  // console.log(result);
   expect(toStandard(result)).toStrictEqual(toStandard(targetCode));
-})
+});
