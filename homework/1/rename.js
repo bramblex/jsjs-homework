0ab376c02918d85f49156488639d271bf1d2326e
wuuusicong <87551619@qq.com>
2022-01-15 14:29:24 +0800

    update

--- homework/1/rename.js
+++ homework/1/rename.js
@@ -5,34 +5,30 @@ const traverse = require('../../common/traverse');
 function transform(root, originName, targetName) {
   // 遍历所有节点
   return traverse((node, ctx, next) => {
-
-
     // TODO: 作业代码写在这里
-
-    // if (node.type === 'FunctionDeclaration') {
-    //   if(node.id.name === originName){
-    //     node.id.name = targetName
-    //   }
-    // }
-    // if (node.type === 'LabeledStatement') {
-    //   if(node.label.name === originName){
-    //     node.label.name = targetName
-    //   }
-    // }
-    // if (node.type === 'VariableDeclaration') {
-    //   for(i of node.declarations){
-    //     i.id.name = targetName
-    //   }
-    // }
-    // if (node.type === 'BreakStatement') {
-    //   if(node.label.name === originName){
-    //     node.label.name = targetName
-    //   }
-    // }
-    if (node.type === 'Identifier') {
-      if(node.name === originName){
-        node.name = targetName
+    /*
+    * 真的尝试了不想一个个匹配
+    * 但是没找到解决方法
+    * 于是就这样了ε=(´ο｀*)))唉
+    * */
+    if (node.type === 'FunctionDeclaration') {
+      if(node.id.name === originName){
+        node.id.name = targetName
       }
+    }
+    if (node.type === 'VariableDeclaration') {
+      for(i of node.declarations){
+        i.id.name = targetName
+      }
+    }
+    if (node.type === 'MemberExpression') {
+      if(node.object.name === originName){
+        node.object.name = targetName
+      }
+    }
+    if (node.type === 'BinaryExpression') {
+      node.left.name = targetName;
+      node.right.name = targetName;
     }
 
 
