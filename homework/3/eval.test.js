d368de0301e8d40c437f93f9d8332d3af2abee5e
kongmoumou <kongmoumou666@gmail.com>
2022-01-23 00:12:35 +0800

    fix: homework 3 test case

--- homework/3/eval.test.js
+++ homework/3/eval.test.js
@@ -6,6 +6,23 @@ function throwError() {
 
 const baseEnv = { throwError }
 
+// ensure result & err equal
+const safeEval = (code) => {
+  try {
+    return [eval(code)]
+  } catch (error) {
+    return [undefined, `${error}`]
+  }
+}
+const safeCustomEval = (code, env) => {
+  try {
+    return [customerEval(code, env)]
+  } catch (error) {
+    // normalize acorn err msg
+    return [undefined, `${error}`.replace(/\s\(\d+:\d+\)$/, '')]
+  }
+}
+
 test('测试声明与控制流 - 初级挑战', () => {
   const sourceCodeList = [
     'var a = 1;let b = 2;const c = 3',
@@ -18,18 +35,24 @@ test('测试声明与控制流 - 初级挑战', () => {
     'const a = 1; let a = 5;'
     ];
   for (sourceCode of sourceCodeList) {
-    expect(customerEval(sourceCode, baseEnv)).toStrictEqual(eval(sourceCode));
+    expect(safeCustomEval(sourceCode, baseEnv)).toStrictEqual(safeEval(sourceCode));
   }
 })
 
 test('测试声明与控制流 - 终极挑战', () => {
-  const sourceCode = [
+  const sourceCodeList = [
     '(() => { let a = 1; var b = 2; (() => { a = 2; b = 3; })(); return { a, b }; })()',
     '((() => { var n = 55; return () => { for (let i = 0; i < 10; i++) { n += i } return n } })())()',
     'const obj = { runTry: false, runError: false, runFinally: false }; try { obj.runTry = true; } catch (err) { obj.runError = true; } finally { obj.runFinally = true; }',
-    '(function t(type) { const result = []; let i = 0; while (i < 5) { i++; switch (type + "") { case "0": continue; }result.push(i); } return result; })(0)'
+    '(function t(type) { const result = []; let i = 0; while (i < 5) { i++; switch (type + "") { case "0": continue; }result.push(i); } return result; })(0)',
+    // add test case
+    '(() => { let a = 1; var b = 2; {let a = 2; var b = 3;} return { a, b }; })()',
+    'const obj = { runTry: false, runError: false, runFinally: false }; try { obj.runTry = true; } catch (err) { obj.runError = true; } finally { obj.runFinally = true; } obj',
+    'const obj = { runTry: false, runError: false, runFinally: false }; try { obj.runTry = true; throw new Error(233);} catch (err) { obj.runError = true; } finally { obj.runFinally = true; }',
   ]
-  expect(customerEval(sourceCode)).toStrictEqual(eval(sourceCode));
+  for (sourceCode of sourceCodeList) {
+    expect(safeCustomEval(sourceCode, baseEnv)).toStrictEqual(safeEval(sourceCode));
+  }
 })
 
 test('测试声明与控制流 - 超纲挑战', () => {
